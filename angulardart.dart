4a0b3b779b0f382a36cecb5985f7f3e4952fd08a
Johan  Antonio <147890607+johansitoweb@users.noreply.github.com>
2024-08-15 22:11:09 +0100

    Add files via upload

---
+++ angulardart.dart
@@ -0,0 +1,11 @@
+// app_component.dart
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'my-app',
+  template: '''
+    <h1>Bienvenido a mi aplicación AngularDart</h1>
+    <p>Esta es una aplicación básica.</p>
+  ''',
+)
+class AppComponent {}
---
+++ appl.dart
@@ -0,0 +1,7 @@
+// main.dart
+import 'package:angular/angular.dart';
+import 'package:my_app/app_component.template.dart' as ng;
+
+void main() {
+  runApp(ng.AppComponentNgFactory);
+}
